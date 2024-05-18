2d89c6180eb10e1346bb08284fa7650d6f190b98
FreerGit <fredrik-98@live.se>
2024-05-18 22:17:49 +0200

    fix: squash unused warnings

--- stx/log.h
+++ stx/log.h
@@ -4,11 +4,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-static const char *event_color = "\x1b[90m";
-static const char *error_color = "\x1b[31m";
-static const char *info_color = "\x1b[32m";
-static const char *fatal_color = "\x1b[35m";
-
 #if defined(LOG_WITH_TIME)
 static struct tm *time_event() {
   time_t t = time(NULL);
@@ -28,13 +23,13 @@ static struct tm *time_event() {
   {}
 #endif // LOG_WITH_TIME
 
-#define log(type, color, format, ...)                                                     \
-  {                                                                                       \
-    log_time();                                                                           \
-    printf("%s%s\x1b[0m %s%s:%d:\x1b[0m ", color, type, event_color, __FILE__, __LINE__); \
-    printf(format, ##__VA_ARGS__);                                                        \
-    printf("\n");                                                                         \
-    fflush(stdout);                                                                       \
+#define log(type, color, format, ...)                                                    \
+  {                                                                                      \
+    log_time();                                                                          \
+    printf("%s%s\x1b[0m %s%s:%d:\x1b[0m ", color, type, "\x1b[90m", __FILE__, __LINE__); \
+    printf(format, ##__VA_ARGS__);                                                       \
+    printf("\n");                                                                        \
+    fflush(stdout);                                                                      \
   }
 
 #if defined(LOG_DEBUG)
@@ -45,14 +40,14 @@ static const char *debug_color = "\x1b[36m";
   {}
 #endif // LOG_DEBUG
 
-#define log_info(format, ...) log("INFO ", info_color, format, ##__VA_ARGS__)
+#define log_info(format, ...) log("INFO ", "\x1b[32m", format, ##__VA_ARGS__)
 
-#define log_error(format, ...) log("ERROR", error_color, format, ##__VA_ARGS__)
+#define log_error(format, ...) log("ERROR", "\x1b[31m", format, ##__VA_ARGS__)
 
-#define log_fatal(format, ...)                        \
-  {                                                   \
-    log("FATAL", fatal_color, format, ##__VA_ARGS__); \
-    exit(-1);                                         \
+#define log_fatal(format, ...)                       \
+  {                                                  \
+    log("FATAL", "\x1b[35m", format, ##__VA_ARGS__); \
+    exit(-1);                                        \
   }
 
 #endif
